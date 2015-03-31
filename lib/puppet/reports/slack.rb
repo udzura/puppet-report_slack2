6e17abda8afb267514ca5dc308a11c05c109b38e
Uchio KONDO <udzura@pepabo.com>
2015-03-31 18:55:47 +0900

    Puppet report works

--- lib/puppet/reports/slack.rb
+++ lib/puppet/reports/slack.rb
@@ -1,5 +1,6 @@
 require 'puppet'
 require 'puppet/network/http_pool'
+require 'net/https'
 require 'uri'
 require 'json'
 
@@ -13,13 +14,16 @@ Puppet::Reports.register_report(:slack) do
 
     return if self.status == "unchanged"
 
+    # Kernel#` should always run on puppetserver host
+    puppetmaster_hostname = `hostname`.chomp
     pretxt = "Puppet status: *%s*" % self.status
-    message = <<-FORMAT % [self.host, Puppet.settings[:environment]]
+    message = <<-FORMAT % [puppetmaster_hostname, self.host, self.environment]
 ```
-Hostname    = %s
-Environment = %s
+Puppet Master Host = %s
+Provisioned Host   = %s
+Run Environment    = %s
 ```
-FORMAT
+    FORMAT
     color = nil
 
     if self.status == "changed"
@@ -35,13 +39,13 @@ FORMAT
     @config["channels"].each do |channel|
       channel.gsub!(/^\\/, '')
       _payload = payload.merge("channel" => channel)
-      params = {"payload" => _payload.to_json}
-      Net::HTTP.post_form(@config["webhook"], params)
+      post_to_webhook(URI.parse(@config["webhook"]), _payload)
+      Puppet.notice("Notification sent to slack channel: #{channel}")
     end
   end
 
   private
-  def make_payload(pretxt, message)
+  def make_payload(pretxt, message, color)
     {
       "username" => (@config["username"] || "puppet"),
       "icon_url" => (@config["icon_url"] || "https://cloud.githubusercontent.com/assets/91011/6860448/96c424a0-d46b-11e4-9131-31a711893171.png"),
@@ -52,5 +56,19 @@ FORMAT
           "color"   => color,
         }],
     }
+  end
+
+  def post_to_webhook(uri, payload)
+    https = Net::HTTP.new(uri.host, 443)
+    https.use_ssl = true
+    r = https.start do |https|
+      https.post(uri.path, payload.to_json)
+    end
+    case r
+    when Net::HTTPSuccess
+      return
+    else
+      Puppet.err("Notification sent faild to slack channel: #{channel}")
+    end
   end
 end
