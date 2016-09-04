8e0c7372fcea31396dd1cd8625742942c3545f5a
dpisano <dpisano@users.noreply.github.com>
2016-09-04 00:57:00 -0400

    Update to automate the set up of Slack notifications

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,30 @@
-class report_slack2 {
+class report_slack2 (
+  $username   = undef,
+  $webhook    = undef,
+  $channels   = [],
+  $report_url = undef,
+) {
+
+  $slack_puppet_confdir = $::puppet_confdir
+
+  $slack = {
+    username    => $username,
+    webhook     => $webhook,
+    channels    => $channels,
+    report_url  => $report_url
+  }
+
+  file { "${slack_puppet_confdir}/slack.yaml":
+    content     => inline_template('<%= YAML.dump(@slack) %>')
+  }
+
+  ini_subsetting { 'puppet.conf/reports/slack':
+    ensure                => present,
+    path                  => "${slack_puppet_confdir}/puppet.conf",
+    section               => 'master',
+    setting               => 'reports',
+    subsetting            => 'slack',
+    subsetting_separator  => ',',
+    require               => File [ "${slack_puppet_confdir}/slack.yaml"],
+  }
 }
